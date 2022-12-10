adde823b1093d5cc83fa7842f35c08eca2a5d3bc
MysteryCoder456 <rehatbir.singh@gmail.com>
2022-12-10 12:18:24 +0400

    cool af widget

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final wp = WordPair.random();
-
     return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
@@ -21,7 +19,22 @@ class MyApp extends StatelessWidget {
                 const TextTheme(bodyMedium: TextStyle(color: Colors.amber))),
         home: Scaffold(
           appBar: AppBar(title: const Text("Flutter Test Poggers")),
-          body: Center(child: Text(wp.asPascalCase)),
+          body: const Center(child: RandomWords()),
         ));
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({super.key});
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wp = WordPair.random();
+    return Text(wp.asPascalCase);
   }
 }
