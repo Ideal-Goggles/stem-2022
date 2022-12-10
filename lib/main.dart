0848b2e30dcb7dc7b1e147e8b5143fe5d8def70c
MysteryCoder456 <rehatbir.singh@gmail.com>
2022-12-10 12:03:22 +0400

    removed comment

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-// import 'package:english_words/english_words.dart';
 
 void main() {
   runApp(const MyApp());
