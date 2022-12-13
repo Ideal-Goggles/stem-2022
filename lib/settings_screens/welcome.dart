930dadf342e47adfcf7a221017f5be0c7456dc89
MysteryCoder456 <rehatbir.singh@gmail.com>
2022-12-13 18:07:57 +0400

    added some welcome tips to welcome screen

--- lib/settings_screens/welcome.dart
+++ lib/settings_screens/welcome.dart
@@ -10,9 +10,33 @@ class WelcomeScreen extends StatefulWidget {
 
 class _WelcomeScreenState extends State<WelcomeScreen> {
   int _current = 0;
+  final _carouselDecoration = BoxDecoration(
+      borderRadius: BorderRadius.circular(8),
+      color: Colors.grey.withOpacity(0.07));
+
+  Widget textPage(String content) {
+    return Container(
+        padding: const EdgeInsets.all(8),
+        decoration: _carouselDecoration,
+        child: Center(
+          child: Text(
+            content,
+            textAlign: TextAlign.center,
+          ),
+        ));
+  }
 
   @override
   Widget build(BuildContext context) {
+    final carouselItems = [
+      textPage(
+          "Hammit is an app that aims to reduce food wastage and junk food consumption."),
+      textPage(
+          "You can post a picture of your food at mealtimes and they will be judged by the community on how healthy they are."),
+      textPage(
+          "Optionally, you can join a school group and compete with other students to become the healthiest student in your school!"),
+    ];
+
     return Scaffold(
         appBar: AppBar(
           title: const Text("Welcome to Hammit"),
@@ -23,37 +47,29 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
             child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Container(
-                margin: const EdgeInsets.only(left: 20, right: 20),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    color: Colors.grey.withOpacity(0.1)),
-                child: CarouselSlider(
-                  options: CarouselOptions(
-                    autoPlay: true,
-                    autoPlayInterval: const Duration(seconds: 15),
-                    onPageChanged: (index, reason) {
-                      setState(() {
-                        _current = index;
-                      });
-                    },
-                  ),
-                  items: const [
-                    Text("hi"),
-                    Text("hello"),
-                    Text("bello"),
-                  ],
-                )),
+            CarouselSlider(
+              options: CarouselOptions(
+                autoPlay: true,
+                autoPlayInterval: const Duration(seconds: 15),
+                enlargeCenterPage: true,
+                height: 260,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    _current = index;
+                  });
+                },
+              ),
+              items: carouselItems,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: [0, 1, 2].map((i) {
-                final color = i == _current ? Colors.grey[700] : Colors.grey;
+              children: carouselItems.asMap().keys.map((i) {
                 return Container(
                     padding:
                         const EdgeInsets.symmetric(horizontal: 3, vertical: 6),
                     child: Icon(
                       Icons.circle,
-                      color: color,
+                      color: i == _current ? Colors.grey[700] : Colors.grey,
                       size: 10,
                     ));
               }).toList(),
