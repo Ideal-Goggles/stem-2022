da39252fe97a3fb589fa2311af0c11d508551c27
Zoclhas <zoclhas@gmail.com>
2022-12-14 17:28:43 +0400

    Styled accounts page- dont check firebase!!

--- lib/settings_screens/sign_up.dart
+++ lib/settings_screens/sign_up.dart
@@ -23,13 +23,44 @@ class _SignUpScreenState extends State<SignUpScreen> {
       FirebaseAuth.instance
           .createUserWithEmailAndPassword(email: email, password: password)
           .then((creds) => creds.user!.updateDisplayName(username))
-          .then((_) => ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(content: Text("Account created successfully!"))))
-          .catchError((error) => ScaffoldMessenger.of(context)
-              .showSnackBar(SnackBar(content: Text(error.toString()))));
+          .then(
+              (_) => ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                    content: Text(
+                      "Account created successfully!",
+                      style: TextStyle(color: Colors.white),
+                      textAlign: TextAlign.center,
+                    ),
+                    backgroundColor: Colors.white12,
+                    margin: EdgeInsets.all(10),
+                    shape: RoundedRectangleBorder(),
+                    behavior: SnackBarBehavior.floating,
+                    elevation: 5,
+                  )))
+          .catchError(
+              (error) => ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text(
+                      error.toString(),
+                      style: TextStyle(color: Colors.white),
+                      textAlign: TextAlign.center,
+                    ),
+                    backgroundColor: Colors.blue,
+                    margin: EdgeInsets.all(15),
+                    shape: StadiumBorder(),
+                    behavior: SnackBarBehavior.floating,
+                    elevation: 5,
+                  )));
 
       ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-          content: Text("Creating account..."),
+          content: Text(
+            "Creating account...",
+            style: TextStyle(color: Colors.white),
+            textAlign: TextAlign.center,
+          ),
+          backgroundColor: Colors.blue,
+          margin: EdgeInsets.all(15),
+          shape: StadiumBorder(),
+          behavior: SnackBarBehavior.floating,
+          elevation: 5,
           duration: Duration(seconds: 2)));
     }
   }
@@ -45,73 +76,113 @@ class _SignUpScreenState extends State<SignUpScreen> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const Text(
-                  "Welcome to Hammit!",
-                  style: TextStyle(fontSize: 20),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: const Text(
+                    "Welcome to Hammit!",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
                 ),
                 const SizedBox(height: 10),
-                TextFormField(
-                  onSaved: (newValue) => username = newValue ?? "",
-                  keyboardType: TextInputType.name,
-                  decoration: const InputDecoration(
-                    labelText: "Username",
-                    hintText: "John Doe",
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Please enter a username.";
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  onSaved: (newValue) => email = newValue ?? "",
-                  keyboardType: TextInputType.emailAddress,
-                  autocorrect: false,
-                  decoration: const InputDecoration(
-                    labelText: "Email address",
-                    hintText: "myemail@mail.com",
-                  ),
-                  validator: (value) {
-                    if (value != null && value.isNotEmpty) {
-                      if (_emailRegex.hasMatch(value)) {
-                        return null;
-                      } else {
-                        return "Please enter a valid email address";
-                      }
-                    }
-                    return "Please enter an email address";
-                  },
-                ),
-                TextFormField(
-                  onSaved: (newValue) => password = newValue ?? "",
-                  keyboardType: TextInputType.visiblePassword,
-                  obscureText: true,
-                  autocorrect: false,
-                  decoration: const InputDecoration(
-                    labelText: "Password",
-                  ),
-                  validator: (value) {
-                    if (value != null && value.isNotEmpty) {
-                      if (value.length < 8) {
-                        return "Password must be at least 8 characters long";
+                Container(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: TextFormField(
+                    onSaved: (newValue) => username = newValue ?? "",
+                    keyboardType: TextInputType.name,
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue.shade600),
+                        borderRadius: BorderRadius.circular(14),
+                      ),
+                      labelText: "Username",
+                      hintText: "John Doe",
+                      labelStyle: TextStyle(color: Colors.blue[600]),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(14),
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter a username.";
                       }
                       return null;
-                    }
-                    return "Please enter a password";
-                  },
+                    },
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: TextFormField(
+                    onSaved: (newValue) => email = newValue ?? "",
+                    keyboardType: TextInputType.emailAddress,
+                    autocorrect: false,
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue.shade600),
+                        borderRadius: BorderRadius.circular(14),
+                      ),
+                      labelText: "Email address",
+                      hintText: "johndoe@example.com",
+                      labelStyle: TextStyle(color: Colors.blue[600]),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(14)),
+                    ),
+                    validator: (value) {
+                      if (value != null && value.isNotEmpty) {
+                        if (_emailRegex.hasMatch(value)) {
+                          return null;
+                        } else {
+                          return "Please enter a valid email address";
+                        }
+                      }
+                      return "Please enter an email address";
+                    },
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: TextFormField(
+                    onSaved: (newValue) => password = newValue ?? "",
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: true,
+                    autocorrect: false,
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue.shade600),
+                        borderRadius: BorderRadius.circular(14),
+                      ),
+                      labelText: "Password",
+                      labelStyle: TextStyle(color: Colors.blue[600]),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(14)),
+                    ),
+                    validator: (value) {
+                      if (value != null && value.isNotEmpty) {
+                        if (value.length < 8) {
+                          return "Password must be at least 8 characters long";
+                        }
+                        return null;
+                      }
+                      return "Please enter a password";
+                    },
+                  ),
                 ),
                 const SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: signUp,
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blue.withOpacity(0.75),
-                      foregroundColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50)),
-                      padding: const EdgeInsets.all(15)),
-                  child: const Text("Sign Up"),
-                ),
+                Container(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    // width:,
+                    onPressed: signUp,
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.grey.withOpacity(0.1),
+                        foregroundColor: Colors.blue[600],
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50)),
+                        padding: const EdgeInsets.all(15)),
+                    child: const Text("Sign Up"),
+                  ),
+                )
               ],
             ),
           ),
