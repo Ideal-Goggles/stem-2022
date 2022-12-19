bffd1ec9ee96e8bdc4b5b0d01223c038a40dbeb8
Zoclhas <70476453+Zoclhas@users.noreply.github.com>
2022-12-19 21:20:18 +0400

    Basic profiles page- reverted the changes done during logout in settings.dart

--- lib/settings_screens/profile.dart
+++ lib/settings_screens/profile.dart
@@ -1,21 +1,83 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:google_sign_in/google_sign_in.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-class ProfileScreen extends StatefulWidget {
+import 'package:stem_2022/tab_screens/settings.dart';
+
+final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
-  State<ProfileScreen> createState() => _ProfileScreenState();
-}
-
-class _ProfileScreenState extends State<ProfileScreen> {
-  @override
   Widget build(BuildContext context) {
+    void logout() async {
+      _googleSignIn.signOut();
+      await FirebaseAuth.instance.signOut();
+
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => SettingsScreen()));
+    }
+
     final currentUser = Provider.of<User?>(context);
     return Scaffold(
         appBar: AppBar(title: Text("Hello, ${currentUser?.displayName}")),
-        body: const Text("mew"));
+        body: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Container(
+                height: 200,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.grey[900],
+                  borderRadius: BorderRadius.all(Radius.circular(50)),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                        flex: 1,
+                        child: const CircleAvatar(
+                          radius: 50,
+                          backgroundImage: NetworkImage(
+                              "https://instagram.ffjr1-4.fna.fbcdn.net/v/t51.2885-19/314209652_1722081581497819_3317807027772244106_n.jpg?stp=dst-jpg_s150x150&_nc_ht=instagram.ffjr1-4.fna.fbcdn.net&_nc_cat=107&_nc_ohc=YwXc-tGHo8UAX-SypNX&edm=AF2UC20BAAAA&ccb=7-5&oh=00_AfCLe3pORcdF-Mc3CKB9parcKzSi17dGoH1DmPEQsZfadA&oe=63A5BA29&_nc_sid=57dc97"),
+                        )),
+                    Expanded(
+                        flex: 1,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "${currentUser?.displayName}",
+                              style: const TextStyle(
+                                  fontSize: 24, fontWeight: FontWeight.w600),
+                            ),
+                            Text("Professional Cat")
+                          ],
+                        )),
+                  ],
+                ),
+              ),
+              SizedBox(height: 10),
+              MaterialButton(
+                  minWidth: double.infinity,
+                  onPressed: logout,
+                  color: Colors.red.withOpacity(0.2),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  elevation: 0,
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 25, bottom: 25),
+                    child: Text(
+                      "Logout",
+                      style: TextStyle(color: Colors.red),
+                    ),
+                  ))
+            ],
+          ),
+        ));
   }
 }
--- lib/tab_screens/settings.dart
+++ lib/tab_screens/settings.dart
@@ -1,22 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:provider/provider.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
 import 'package:stem_2022/settings_screens/welcome.dart';
 import 'package:stem_2022/settings_screens/sign_up.dart';
 import 'package:stem_2022/settings_screens/login.dart';
 import 'package:stem_2022/settings_screens/profile.dart';
 
-final GoogleSignIn _googleSignIn = GoogleSignIn();
-
 class SettingsMenuEntry {
   final IconData icon;
   final String text;
-  final Widget? destination;
-  final Function? action;
+  final Widget destination;
 
-  SettingsMenuEntry(this.icon, this.text, this.destination, this.action);
+  SettingsMenuEntry(this.icon, this.text, this.destination);
 }
 
 class SettingsScreen extends StatelessWidget {
@@ -27,24 +23,16 @@ class SettingsScreen extends StatelessWidget {
     User? user = Provider.of<User?>(context);
     bool loggedIn = user != null;
 
-    void logout() async {
-      _googleSignIn.signOut();
-      await FirebaseAuth.instance.signOut();
-    }
-
     final List<SettingsMenuEntry> entries = [
-      SettingsMenuEntry(
-          Icons.help, "What is Hammit?", const WelcomeScreen(), null),
+      SettingsMenuEntry(Icons.help, "What is Hammit?", const WelcomeScreen()),
       if (!loggedIn) ...[
         SettingsMenuEntry(Icons.perm_contact_calendar, "Create an Account",
-            const SignUpScreen(), null),
+            const SignUpScreen()),
         SettingsMenuEntry(
-            Icons.login, "Log Into Existing Account", const LoginScreen(), null)
+            Icons.login, "Log Into Existing Account", const LoginScreen())
       ],
-      if (loggedIn) ...[
-        SettingsMenuEntry(Icons.person, "Profile", const ProfileScreen(), null),
-        SettingsMenuEntry(Icons.exit_to_app, "Logout", null, logout),
-      ]
+      if (loggedIn)
+        SettingsMenuEntry(Icons.person, "Profile", const ProfileScreen()),
     ];
 
     return Container(
@@ -56,16 +44,12 @@ class SettingsScreen extends StatelessWidget {
           itemBuilder: (BuildContext context, int index) {
             return MaterialButton(
               onPressed: () {
-                if (entries[index].destination != null) {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => entries[index].destination!,
-                    ),
-                  );
-                } else if (entries[index].action != null) {
-                  entries[index].action!.call();
-                }
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => entries[index].destination,
+                  ),
+                );
               },
               color: Colors.grey.withOpacity(0.1),
               shape: RoundedRectangleBorder(
