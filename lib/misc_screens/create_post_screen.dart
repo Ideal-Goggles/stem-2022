08af0bca528acef1343392dc45d0bfc8dce7f01d
MysteryCoder456 <rehatbir.singh@gmail.com>
2022-12-20 23:32:03 +0400

    basic form UI

--- lib/misc_screens/create_post_screen.dart
+++ lib/misc_screens/create_post_screen.dart
@@ -10,7 +10,10 @@ class CreatePostScreen extends StatefulWidget {
 }
 
 class _CreatePostScreenState extends State<CreatePostScreen> {
+  final _formKey = GlobalKey<FormState>();
   int _secondsLeft = 60;
+
+  String _caption = "";
 
   Future timer() async {
     while (_secondsLeft > 0) {
@@ -30,7 +33,59 @@ class _CreatePostScreenState extends State<CreatePostScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Create a Post")),
-      body: Center(child: Text("$_secondsLeft")),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(50),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Create a new post",
+                style: TextStyle(fontSize: 20),
+              ),
+              const SizedBox(height: 10),
+              Container(
+                padding: const EdgeInsets.only(top: 20),
+                child: TextFormField(
+                  onSaved: (newValue) => _caption = newValue ?? "",
+                  keyboardType: TextInputType.text,
+                  autocorrect: true,
+                  decoration: const InputDecoration(
+                    labelText: "Caption",
+                    hintText: "An amazing meal!",
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Please enter an email address";
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                width: double.infinity,
+                child: MaterialButton(
+                  onPressed: () {},
+                  color: Colors.grey[900],
+                  textColor: Theme.of(context).colorScheme.primary,
+                  shape: const StadiumBorder(),
+                  padding: const EdgeInsets.all(15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(Icons.camera_alt, size: 25),
+                      SizedBox(width: 5),
+                      Text("Click a Picture"),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
