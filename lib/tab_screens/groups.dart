a3087431e36a83a9a7873af0fb9158061178cfa1
Zoclhas <70476453+Zoclhas@users.noreply.github.com>
2022-12-22 20:44:56 +0400

    removed unused dependencies

--- lib/tab_screens/groups.dart
+++ lib/tab_screens/groups.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:provider/provider.dart';
 
 import 'package:stem_2022/models/group.dart';
