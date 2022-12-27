05ec5e338c9cfc02043162da06b9bf4133cf0c97
MysteryCoder456 <rehatbir.singh@gmail.com>
2022-12-27 13:16:44 +0400

    added join group screen ui

--- lib/settings_screens/join_group.dart
+++ lib/settings_screens/join_group.dart
@@ -8,9 +8,72 @@ class JoinGroupScreen extends StatefulWidget {
 }
 
 class _JoinGroupScreenState extends State<JoinGroupScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  String groupCode = "";
+
+  void showJoinGroupDialog() {
+    // TODO
+  }
+
+  void joinGroup() {
+    // TODO
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Join a Group"),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(50),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Enter Group Code",
+                style: TextStyle(fontSize: 20),
+              ),
+              const SizedBox(height: 30),
+              TextFormField(
+                onSaved: (newValue) => groupCode = newValue ?? "",
+                keyboardType: TextInputType.number,
+                autocorrect: false,
+                maxLength: 4,
+                decoration: const InputDecoration(
+                  labelText: "Group Code",
+                  hintText: "1234",
+                ),
+                validator: (value) {
+                  if (value != null && value.isNotEmpty) {
+                    if (int.tryParse(value) != null && value.length == 4) {
+                      return null;
+                    } else {
+                      return "Code must be a 4-digit number";
+                    }
+                  }
+                  return "Please enter a code";
+                },
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                width: double.infinity,
+                child: MaterialButton(
+                  onPressed: showJoinGroupDialog,
+                  color: Colors.grey[900],
+                  textColor: Theme.of(context).colorScheme.primary,
+                  shape: const StadiumBorder(),
+                  padding: const EdgeInsets.all(15),
+                  child: const Text("Join"),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
