0f6ab565216eee09c7cab6fd16af6c5e248767be
MysteryCoder456 <rehatbir.singh@gmail.com>
2022-12-27 12:29:04 +0400

    new settings menu entry

---
+++ lib/settings_screens/join_group.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class JoinGroupScreen extends StatefulWidget {
+  const JoinGroupScreen({super.key});
+
+  @override
+  State<JoinGroupScreen> createState() => _JoinGroupScreenState();
+}
+
+class _JoinGroupScreenState extends State<JoinGroupScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
