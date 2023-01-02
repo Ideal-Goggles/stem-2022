5680b6dce0017ec0b1fa89680356cf67e714292c
MysteryCoder456 <rehatbir.singh@gmail.com>
2023-01-02 21:52:36 +0400

    post long press haptic feedback

--- lib/tab_screens/home.dart
+++ lib/tab_screens/home.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:event/event.dart';
@@ -315,7 +316,10 @@ class _FoodPostCardState extends State<FoodPostCard>
 
     return MaterialButton(
       onPressed: null,
-      onLongPress: showDeletionDialog,
+      onLongPress: () {
+        HapticFeedback.mediumImpact();
+        showDeletionDialog();
+      },
       padding: EdgeInsets.zero,
       child: Card(
         color: Colors.grey[900],
