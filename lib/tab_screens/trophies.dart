f83db61dbf32bed4a705272baf7c86c659aeae7a
MysteryCoder456 <rehatbir.singh@gmail.com>
2023-01-04 16:01:49 +0400

    auth check

--- lib/tab_screens/trophies.dart
+++ lib/tab_screens/trophies.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class TrophiesScreen extends StatelessWidget {
   const TrophiesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    final currentUser = Provider.of<User?>(context);
+
+    if (currentUser == null) {
+      return const Center(
+        child: Text("Please login to access trophies"),
+      );
+    }
+
+    return const Center(child: Text("Coming soon..."));
   }
 }
