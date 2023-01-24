f443190b4230a2c04f57cb257ae4a2e9ccf6e72c
Zoclhas <zoclhas@gmail.com>
2023-01-24 16:04:22 +0400

    principal view stuff

--- lib/tab_screens/my_group.dart
+++ lib/tab_screens/my_group.dart
@@ -67,7 +67,7 @@ class MyGroupScreen extends StatelessWidget {
           }
 
           if (group.admin == appUser.id) {
-            return const PrincipalView(
+            return PrincipalView(
               groupId: group.id,
             );
           }
@@ -856,15 +856,169 @@ class _SupervisorViewState extends State<SupervisorView> {
 
 class PrincipalView extends StatefulWidget {
   final String groupId;
-  const PrincipalView({super.key, required this.groupId});
+  List sections = ["Junior", "Secondary", "Senior"];
+  PrincipalView({super.key, required this.groupId});
 
   @override
   State<PrincipalView> createState() => _PrincipalViewState();
 }
 
 class _PrincipalViewState extends State<PrincipalView> {
+  late final Map<String, double> _gradeWastage;
+  late final Map<String, List<double>> _gradeHealth;
+  late final Map<String, double> _subGroupWastage;
+  late final Map<String, List<double>> _subGroupHealth;
+
+  late final Map<String, double> _gradeWastageForYear;
+  late final Map<String, List<double>> _gradeHealthForYear;
+  late final Map<String, double> _subGroupWastageForYear;
+  late final Map<String, List<double>> _subGroupHealthForYear;
+
+  late final Map<String, List<Map<String, Object>>> sectionData;
+  late final Map<String, List<Map<String, Object>>> sectionDataForYear;
+
+  bool _loading = true;
+
+  TextStyle get _bodyTextStyle => TextStyle(
+        color: Colors.grey.shade300,
+        fontSize: 15,
+      );
+  Divider get _divider => const Divider(thickness: 1, color: Colors.white38);
+
+  @override
+  void initState() {
+    final db = Provider.of<DatabaseService>(context, listen: false);
+
+    for (final section in widget.sections) {
+      db.getSectionSubGroups(widget.groupId, section).then(
+        (subGroups) async {
+          Map<String, double> gradeWastage = {};
+          Map<String, List<double>> gradeHealth = {};
+          Map<String, double> subGroupWastage = {};
+          Map<String, List<double>> subGroupHealth = {};
+
+          Map<String, double> gradeWastageForYear = {};
+          Map<String, List<double>> gradeHealthForYear = {};
+          Map<String, double> subGroupWastageForYear = {};
+          Map<String, List<double>> subGroupHealthForYear = {};
+
+          for (final subGroup in subGroups) {
+            final subGroupGrade =
+                subGroup.id.substring(0, subGroup.id.length - 2);
+
+            // Fetch data
+            final wastageFuture =
+                db.getWastageData(widget.groupId, subGroup.id);
+            final wastageForYearFuture = db.getWastageDataForYear(
+              widget.groupId,
+              subGroup.id,
+              year: DateTime.now().year,
+            );
+
+            final healthFuture = db.getHealthData(widget.groupId, subGroup.id);
+            final healthForYearFuture = db.getHealthDataForYear(
+              widget.groupId,
+              subGroup.id,
+              year: DateTime.now().year,
+            );
+
+            // Process wastage data
+            for (final wastage in await wastageFuture) {
+              gradeWastage.update(
+                subGroupGrade,
+                (w) => w + wastage.totalWastage,
+                ifAbsent: () => wastage.totalWastage,
+              );
+              subGroupWastage.update(
+                subGroup.id,
+                (w) => w + wastage.totalWastage,
+                ifAbsent: () => wastage.totalWastage,
+              );
+            }
+
+            // Process wastage for year data
+            for (final wastage in await wastageForYearFuture) {
+              gradeWastageForYear.update(
+                subGroupGrade,
+                (w) => w + wastage.totalWastage,
+                ifAbsent: () => wastage.totalWastage,
+              );
+              subGroupWastageForYear.update(
+                subGroup.id,
+                (w) => w + wastage.totalWastage,
+                ifAbsent: () => wastage.totalWastage,
+              );
+            }
+
+            // Process health data
+            for (final health in await healthFuture) {
+              gradeHealth.update(
+                subGroupGrade,
+                (h) => [...h, health.healthyPercent],
+                ifAbsent: () => [health.healthyPercent],
+              );
+              subGroupHealth.update(
+                subGroup.id,
+                (h) => [...h, health.healthyPercent],
+                ifAbsent: () => [health.healthyPercent],
+              );
+            }
+            for (final health in await healthForYearFuture) {
+              gradeHealthForYear.update(
+                subGroupGrade,
+                (h) => [...h, health.healthyPercent],
+                ifAbsent: () => [health.healthyPercent],
+              );
+              subGroupHealthForYear.update(
+                subGroup.id,
+                (h) => [...h, health.healthyPercent],
+                ifAbsent: () => [health.healthyPercent],
+              );
+            }
+          }
+
+          // Process health for year data
+
+          // Update state after all data has been processed
+          setState(() {
+            _gradeWastage = gradeWastage;
+            _gradeHealth = gradeHealth;
+            _subGroupWastage = subGroupWastage;
+            _subGroupHealth = subGroupHealth;
+
+            _gradeWastageForYear = gradeWastageForYear;
+            _gradeHealthForYear = gradeHealthForYear;
+
+            _loading = false;
+          });
+
+          sectionData[section] = [gradeWastage, gradeHealth];
+        },
+      );
+    }
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Principal View"));
+    if (_loading) {
+      return const Center(child: CircularProgressIndicator());
+    }
+
+    return ListView(
+      children: [
+        for (final section in widget.sections)
+          ExpansionTile(
+            title: Text("$section"),
+            childrenPadding: const EdgeInsets.all(15),
+            children: [
+              SizedBox(
+                height: 205,
+              )
+            ],
+          )
+      ],
+    );
   }
 }
